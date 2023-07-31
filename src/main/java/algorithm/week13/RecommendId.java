c8a60a8760839c117c4f44265b0c1587f380824c
lay423 <junpang486@gmail.com>
2023-07-31 12:14:36 +0900

    아이디 추천 `review`

---
+++ src/main/java/algorithm/week13/RecommendId.java
@@ -0,0 +1,30 @@
+package algorithm.week13;
+
+class RecommendId {
+  public String solution(String new_id) {
+    String answer = new_id;
+
+    answer = answer.toLowerCase();
+    answer = answer.replaceAll("[^a-z0-9-_.]","");
+    answer = answer.replaceAll("[.]{2,}",".");
+    answer = answer.replaceAll("^[.]|[.]$","");
+
+    if(answer.equals(""))
+      answer += "a";
+
+    if(answer.length()>15){
+      answer = answer.substring(0, 15);
+      answer = answer.replaceAll("[.]$","");
+    }
+
+
+    if(answer.length()==1){
+      String str = answer + answer;
+      answer = str + answer;
+    }else if(answer.length()==2){
+      answer = answer + answer.charAt(1);
+    }
+
+    return answer;
+  }
+}
