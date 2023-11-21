ce35d0806b9a855702c411096deb93dd96ec9ab8
lay423 <junpang486@gmail.com>
2023-11-21 11:14:04 +0900

    우물안 개구리 `review`

---
+++ src/main/java/algorithm/week28/java1121/FrogInTheWell.java
@@ -0,0 +1,56 @@
+package algorithm.week28.java1121;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class FrogInTheWell
+{
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int[] arrOfWeight = new int[N + 1];
+    int result = 0;
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      arrOfWeight[i] = Integer.parseInt(st.nextToken());
+    }
+
+    List<Stack<Integer>> listOfStacks = new ArrayList<>();
+
+    for (int i = 0; i <= N; i++) {
+      listOfStacks.add(new Stack<Integer>());
+    }
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int U = Integer.parseInt(st.nextToken());
+      int V = Integer.parseInt(st.nextToken());
+      listOfStacks.get(U).add(V);
+      listOfStacks.get(V).add(U);
+    }
+
+    for (int i = 1; i <= N; i++) {
+      while (!listOfStacks.get(i).isEmpty() && arrOfWeight[listOfStacks.get(i).peek()] < arrOfWeight[i]) {
+        listOfStacks.get(i).pop();
+      }
+      if (listOfStacks.get(i).isEmpty()) {
+        result++;
+      }
+    }
+
+    bw.write(String.valueOf(result));
+    bw.flush();
+
+  }
+}
