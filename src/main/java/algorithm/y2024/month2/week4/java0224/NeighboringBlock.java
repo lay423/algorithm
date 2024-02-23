606a22d4746c1b3a95208c380c595683b265ede5
lay423 <junpang486@gmail.com>
2024-02-24 02:16:24 +0900

    이웃한 칸 `review`

---
+++ src/main/java/algorithm/y2024/month2/week4/java0224/NeighboringBlock.java
@@ -0,0 +1,21 @@
+package algorithm.y2024.month2.week4.java0224;
+
+class NeighboringBlock {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        int n = board.length;
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{-1, 0, 1, 0};
+
+        for(int i=0; i<4; i++){
+            int nx = dx[i] + h;
+            int ny = dy[i] + w;
+            if(0<= nx && nx <n && 0<= ny && ny < n){
+                if(board[h][w].equals(board[nx][ny])){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
