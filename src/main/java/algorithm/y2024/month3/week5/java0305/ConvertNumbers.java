e4aec0df568b3b5dbb0fda574d291fd10243b7e4
lay423 <junpang486@gmail.com>
2024-03-05 02:52:00 +0900

    숫자 변환하기

---
+++ src/main/java/algorithm/y2024/month3/week5/java0305/ConvertNumbers.java
@@ -0,0 +1,23 @@
+package algorithm.y2024.month3.week5.java0305;
+
+//숫자 변환하기
+class ConvertNumbers {
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        int[] dp = new int[y+1];
+        for(int i=x; i<=y; i++){
+            if(i !=x && dp[i] == 0){
+                dp[i] = -1;
+                continue;
+            }
+            if(i*2 <=y){
+                dp[i*2] = (dp[i*2] == 0) ? dp[i]+1 : Math.min(dp[i]+1, dp[i*2]);
+            }
+            if(i*3 <=y)
+                dp[i*3] = (dp[i*3] == 0) ? dp[i]+1 : Math.min(dp[i]+1, dp[i*3]);
+            if(i+n <=y)
+                dp[i+n] = (dp[i+n] == 0) ? dp[i]+1 : Math.min(dp[i]+1, dp[i+n]);
+        }
+        return dp[y];
+    }
+}
