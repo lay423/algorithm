88c1f61dbf8bc94a97ba6623bbff9fcdeb036871
lay423 <junpang486@gmail.com>
2024-03-05 01:23:57 +0900

    스킬트리

---
+++ src/main/java/algorithm/y2024/month3/week5/java0305/SkillTree.java
@@ -0,0 +1,34 @@
+package algorithm.y2024.month3.week5.java0305;
+
+import java.util.*;
+
+class SkillTree {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        List<Character> list = new ArrayList<>();
+        for(int i=0; i<skill.length(); i++){
+            list.add(skill.charAt(i));
+        }
+
+        for(String tree : skill_trees){
+            int idx = 0;
+            boolean flag = false;
+            for(int i=0; i<tree.length(); i++){
+
+                if(idx < skill.length() &&
+                        list.contains(tree.charAt(i)) && tree.charAt(i) != skill.charAt(idx)){
+                    flag = true;
+                    break;
+                }
+                if(idx < skill.length() && tree.charAt(i) == skill.charAt(idx)){
+                    idx++;
+                }
+            }
+            if(!flag){
+                answer++;
+            }
+
+        }
+        return answer;
+    }
+}
