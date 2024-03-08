9187ce1aa7e5e981b6071fb6718f32ec64c45955
lay423 <junpang486@gmail.com>
2024-03-08 09:35:57 +0900

    두 큐 합 같게 만들기

---
+++ src/main/java/algorithm/y2024/month3/week6/java0308/SumOfTwoQueues.java
@@ -0,0 +1,41 @@
+package algorithm.y2024.month3.week6.java0308;
+
+import java.util.*;
+
+//두 큐 합 같게 만들기
+class SumOfTwoQueues {
+    public int solution(int[] queue1, int[] queue2) {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        long sum1=0;
+        long sum2=0;
+        int answer = 0;
+        for(int num : queue1){
+            q1.add(num);
+            sum1 += num;
+        }
+        for(int num : queue2){
+            q2.add(num);
+            sum2 += num;
+        }
+        while(sum1 != sum2){
+            if(answer > (queue1.length + queue2.length) *2){
+                return -1;
+            }
+            int val = 0;
+            if(sum1 > sum2){
+                val = q1.poll();
+                q2.add(val);
+                sum1 -= val;
+                sum2 += val;
+            }else{
+                val = q2.poll();
+                q1.add(val);
+                sum1 += val;
+                sum2 -= val;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
