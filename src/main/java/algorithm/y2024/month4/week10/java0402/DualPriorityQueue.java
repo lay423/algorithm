a754735d8eb6f1f1c50ffba6b6bae8aeac09fd58
lay423 <junpang486@gmail.com>
2024-04-02 14:46:16 +0900

    이중 우선순위 큐

---
+++ src/main/java/algorithm/y2024/month4/week10/java0402/DualPriorityQueue.java
@@ -0,0 +1,31 @@
+package algorithm.y2024.month4.week10.java0402;
+
+
+import java.util.*;
+
+//이중 우선순위 큐
+class DualPriorityQueue {
+    public int[] solution(String[] operations) {
+        List<Integer> list = new ArrayList<>();
+        for(String oper : operations){
+            String[] split = oper.split(" ");
+            if(split[0].equals("I")){
+                int num = Integer.parseInt(split[1]);
+                list.add(num);
+                Collections.sort(list);
+                continue;
+            }
+            if(list.isEmpty())
+                continue;
+            if(split[1].charAt(0) == '-'){
+                list.remove(0);
+            }else{
+                list.remove(list.size()-1);
+            }
+        }
+        if(list.isEmpty())
+            return new int[]{0, 0};
+
+        return new int[]{list.get(list.size()-1), list.get(0)};
+    }
+}
