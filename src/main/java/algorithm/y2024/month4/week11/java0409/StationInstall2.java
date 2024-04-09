822446897d630ae189accc4503f37f9eae69af1a
lay423 <junpang486@gmail.com>
2024-04-09 14:55:20 +0900

    기지국 설치2

---
+++ src/main/java/algorithm/y2024/month4/week11/java0409/StationInstall2.java
@@ -0,0 +1,30 @@
+package algorithm.y2024.month4.week11.java0409;
+
+//기지국 설치
+
+class StationInstall2 {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int next = 1;
+        int block = 2 * w + 1;
+
+        for(int station : stations){
+            int start = station - w;
+            int end = station + w;
+            if(next < start){
+                int dist = start - next;
+                answer += dist / block;
+                if(dist % block != 0)
+                    answer += 1;
+            }
+            next = end + 1;
+        }
+        if(next <= n){
+            int dist = n - next + 1;
+            answer += dist / block;
+            if(dist % block != 0)
+                answer += 1;
+        }
+        return answer;
+    }
+}
