791f89a7c3fb1b6d402515cd84bae1543e7122d2
lay423 <junpang486@gmail.com>
2024-04-12 15:13:26 +0900

    불량 사용자 `review`

---
+++ src/main/java/algorithm/y2024/month4/week11/java0412/RogueUser.java
@@ -0,0 +1,42 @@
+package algorithm.y2024.month4.week11.java0412;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+//불량 사용자
+
+class RogueUser {
+    static boolean[] check;
+    static HashSet<String> set;
+    public int solution(String[] userid, String[] banid) {
+        check = new boolean[userid.length];
+        set = new HashSet<String>();
+
+        for(int i=0; i<banid.length; i++)
+            banid[i] = banid[i].replace('*', '.');
+
+        back(0, "", banid, userid);
+
+        return set.size();
+    }
+
+    public static void back(int depth, String res, String[] banid, String[] userid){
+        if(depth == banid.length){
+            String[] arr = res.split(" ");
+            Arrays.sort(arr);
+
+            String str="";
+            for(String s: arr) str+=s;
+            set.add(str);
+
+            return;
+        }
+
+        for(int i=0; i<userid.length; i++){
+            if(check[i] || !userid[i].matches(banid[depth])) continue;
+            check[i] = true;
+            back(depth+1, userid[i]+" "+res,banid, userid);
+            check[i] = false;
+        }
+    }
+}
