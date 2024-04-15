5d8b7258e2eb4142554abe5d2e8c79c174090ca7
lay423 <junpang486@gmail.com>
2024-04-15 13:15:07 +0900

    섬 연결하기

---
+++ src/main/java/algorithm/y2024/month4/week12/java0415/ConnectingIsland.java
@@ -0,0 +1,38 @@
+package algorithm.y2024.month4.week12.java0415;
+
+import java.util.*;
+
+//섬 연결하기
+
+class ConnectingIsland {
+    public int findParent(int[] parent, int node){
+        if(parent[node] == node)
+            return node;
+        return findParent(parent, parent[node]);
+    }
+    public void union(int[] parent, int node1, int node2){
+        int p1 = findParent(parent, node1);
+        int p2 = findParent(parent, node2);
+
+        if(p1 < p2)
+            parent[p2] = p1;
+        else
+            parent[p1] = p2;
+    }
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        int[] parent = new int[n];
+
+        for(int i=0; i<parent.length; i++)
+            parent[i] = i;
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+
+        for(int i=0; i<costs.length; i++){
+            if(findParent(parent, costs[i][0]) != findParent(parent, costs[i][1])){
+                answer += costs[i][2];
+                union(parent, costs[i][0], costs[i][1]);
+            }
+        }
+        return answer;
+    }
+}
